e5b613d9699a9bab47af9e0ef0e12a656c02c4e2
Ignatius Reza <lyoneil.de.sire@gmail.com>
2019-11-04 10:56:20 +0900

    configure prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
     jest: true,
     node: true,
   },
-  extends: 'eslint:recommended',
+  extends: ['eslint:recommended', 'prettier'],
   parser: 'babel-eslint',
   parserOptions: {
     ecmaVersion: 2018,
@@ -15,7 +15,5 @@ module.exports = {
   rules: {
     'comma-dangle': ['error', 'always-multiline'],
     'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-    indent: ['error', 2],
-    quotes: ['error', 'single'],
   },
 };
